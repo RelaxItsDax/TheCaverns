a07bf71606d8c100279758fb7371f56182406bb4
BuildTools <unconfigured@null.spigotmc.org>
2023-06-02 08:36:42 -0400

    Separate runnables for health calculations, mana calculations, display, absorption, and health regen

--- src/main/java/me/relaxitsdax/thecaverns/PlayerData/PassivePlayerLoop.java
+++ src/main/java/me/relaxitsdax/thecaverns/PlayerData/PassivePlayerLoop.java
@@ -16,30 +16,44 @@ public class PassivePlayerLoop {
         PlayerData data = DataManager.get(uuid);
         Player player = TheCaverns.getInstance().getServer().getPlayer(uuid);
 
+        //Health and Mana Display
         new BukkitRunnable() {
             @Override
             public void run() {
-
                 data.setEffectiveHealth(data.getHealth() + data.getBarrier());
-
 
 
                 String healthInBar = (data.getBarrier() > 0 ? ChatColor.GOLD : ChatColor.RED) + "" + (int) data.getEffectiveHealth();
                 String actionBar = ChatColor.RED + "Health: " + healthInBar + ChatColor.RED + " / " + (int) data.getMaxHealth() + "   " + ChatColor.AQUA + "Mana: " + (int) data.getMana() + " / " + (int) data.getMaxMana();
                 player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(actionBar));
+            }
+        }.runTaskTimer(TheCaverns.getInstance(), 0, 5);
 
-                //Health Calculations
+        //Health Calculations
+        new BukkitRunnable() {
+            @Override
+            public void run() {
                 double healthFraction = (data.getHealth() / data.getMaxHealth());
                 player.setHealth(data.getHealth() == data.getMaxHealth() ? 20 : healthFraction >= 0.1 ? Math.floor(20 * healthFraction) : 1);
                 //If health is equal to max health, then set hearts to 20, otherwise if the hearts are less than .5, set hearts to 0.5, otherwise do the hearts calc
                 data.setEffectiveHealth(data.getHealth() + data.getBarrier());
+            }
+        }.runTaskTimer(TheCaverns.getInstance(), 0, 5);
 
-                //Mana Calculations
+        //Mana Calculations
+        new BukkitRunnable() {
+            @Override
+            public void run() {
                 double manaFraction = data.getMana() / data.getMaxMana();
                 player.setExp((float) manaFraction);
                 player.setLevel(0);
+            }
+        }.runTaskTimer(TheCaverns.getInstance(), 0, 5);
 
-
+        //Absorption
+        new BukkitRunnable() {
+            @Override
+            public void run() {
                 if (data.getBarrier() > 0) {
                     double barrierFraction = (data.getBarrier() / data.getMaxHealth());
                     player.setAbsorptionAmount(data.getBarrier() == data.getMaxHealth() ? 20 : barrierFraction >= 0.1 ? Math.floor(20 * barrierFraction) : 1);
@@ -53,12 +67,10 @@ public class PassivePlayerLoop {
                 if (data.getBarrier() <= 0) {
                     data.setBarrier(0);
                 }
-
-
-
             }
         }.runTaskTimer(TheCaverns.getInstance(), 0, 5);
 
+        //Health Regen
         new BukkitRunnable() {
             @Override
             public void run() {
@@ -69,7 +81,6 @@ public class PassivePlayerLoop {
                 }
             }
         }.runTaskTimer(TheCaverns.getInstance(), 0, 20);
-
 
     }
 
